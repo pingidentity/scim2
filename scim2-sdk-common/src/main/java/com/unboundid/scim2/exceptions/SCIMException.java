6907dafeee1d374f94e3f67d60106e0b432c512a
Bo Li <bo.li@unboundid.com>
2015-04-06 17:19:16 -0500

    Rename SCIMException to ScimException for consistency.

--- scim2-sdk-common/src/main/java/com/unboundid/scim2/exceptions/SCIMException.java
+++ scim2-sdk-common/src/main/java/com/unboundid/scim2/exceptions/SCIMException.java
@@ -21,25 +21,28 @@ package com.unboundid.scim2.exceptions;
  * This class is the base class for all custom checked exceptions defined in
  * the SCIM SDK.
  */
-public class SCIMException extends Exception
+public class ScimException extends Exception
 {
   /**
    * The HTTP status code for this SCIM exception.
    */
   private final int statusCode;
 
-
+  private final String scimType;
 
   /**
    * Create a new SCIM exception from the provided information.
    *
    * @param statusCode    The HTTP status code for this SCIM exception.
+   * @param scimType      The SCIM detailed error keyword.
    * @param errorMessage  The error message for this SCIM exception.
    */
-  public SCIMException(final int statusCode, final String errorMessage)
+  public ScimException(final int statusCode, final String scimType,
+                       final String errorMessage)
   {
     super(errorMessage);
 
+    this.scimType = scimType;
     this.statusCode = statusCode;
   }
 
@@ -49,17 +52,20 @@ public class SCIMException extends Exception
    * Create a new SCIM exception from the provided information.
    *
    * @param statusCode    The HTTP status code for this SCIM exception.
+   * @param scimType      The SCIM detailed error keyword.
    * @param errorMessage  The error message for this SCIM exception.
    * @param cause         The cause (which is saved for later retrieval by the
    *                      {@link #getCause()} method).  (A <tt>null</tt> value
    *                      is permitted, and indicates that the cause is
    *                      nonexistent or unknown.)
    */
-  public SCIMException(final int statusCode, final String errorMessage,
-                          final Throwable cause)
+  public ScimException(final int statusCode, final String scimType,
+                       final String errorMessage,
+                       final Throwable cause)
   {
     super(errorMessage, cause);
 
+    this.scimType = scimType;
     this.statusCode = statusCode;
   }
 
@@ -76,48 +82,13 @@ public class SCIMException extends Exception
   }
 
   /**
-   * Create the appropriate SCIMException from the provided information.
+   * Retrieve the SCIM detailed error keyword.
    *
-   * @param statusCode    The HTTP status code for this SCIM exception.
-   * @param errorMessage  The error message for this SCIM exception.
-   * @return The appropriate SCIMException from the provided information.
+   * @return  The SCIM detailed error keyword.
    */
-  public static SCIMException createException(final int statusCode,
-                                              final String errorMessage)
+  public String getScimType()
   {
-    return createException(statusCode, errorMessage, null);
-  }
-
-  /**
-   * Create the appropriate SCIMException from the provided information.
-   *
-   * @param statusCode    The HTTP status code for this SCIM exception.
-   * @param errorMessage  The error message for this SCIM exception.
-   * @param cause         The cause (which is saved for later retrieval by the
-   *                      {@link #getCause()} method).  (A <tt>null</tt> value
-   *                      is permitted, and indicates that the cause is
-   *                      nonexistent or unknown.)
-   * @return The appropriate SCIMException from the provided information.
-   */
-  public static SCIMException createException(final int statusCode,
-                                              final String errorMessage,
-                                              final Exception cause)
-  {
-    switch(statusCode)
-    {
-//      case -1  : return new ConnectException(errorMessage);
-      case 304 : return new NotModifiedException(errorMessage);
-      case 400 : return new InvalidResourceException(errorMessage);
-      case 401 : return new UnauthorizedException(errorMessage);
-      case 403 : return new ForbiddenException(errorMessage);
-      case 404 : return new ResourceNotFoundException(errorMessage);
-      case 409 : return new ResourceConflictException(errorMessage);
-      case 412 : return new PreconditionFailedException(errorMessage);
-//      case 413 : return new RequestEntityTooLargeException(errorMessage);
-      case 500 : return new ServerErrorException(errorMessage);
-//      case 501 : return new UnsupportedOperationException(errorMessage);
-      default : return new SCIMException(statusCode, errorMessage, cause);
-    }
+    return scimType;
   }
 
 }
