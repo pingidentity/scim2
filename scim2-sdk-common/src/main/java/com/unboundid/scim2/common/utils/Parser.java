96f28748b667da5ccfe9a61988c041400fa979e3
Jacob C <jacobc@gmail.com>
2016-04-13 10:31:47 -0500

    Revert "Fix bug in which a filter could not be parsed if it used a sub-attribute name beginning with the '$' character, which is conventionally used for reference sub-attributes."
    This reverts commit 0a4f027b876aa28eca7da07411fbe5e0ea5db721.

--- scim2-sdk-common/src/main/java/com/unboundid/scim2/common/utils/Parser.java
+++ scim2-sdk-common/src/main/java/com/unboundid/scim2/common/utils/Parser.java
@@ -425,7 +425,7 @@ public class Parser
         }
         return b.toString();
       }
-      if (c == '-' || c == '_' || c == '.' || c == ':' || c == '$' ||
+      if (c == '-' || c == '_' || c == '.' || c == ':' ||
           Character.isLetterOrDigit(c))
       {
         b.append((char)c);
