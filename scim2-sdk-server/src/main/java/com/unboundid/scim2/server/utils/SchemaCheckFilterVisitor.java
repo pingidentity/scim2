c9e93608c68ed5e48b4e02246fe7e9538bd93b0c
Khalid Qarryzada <khalidqarryzada@pingidentity.com>
2025-01-01 11:04:36 -0600

    Update copyright years to include 2025.

--- scim2-sdk-server/src/main/java/com/unboundid/scim2/server/utils/SchemaCheckFilterVisitor.java
+++ scim2-sdk-server/src/main/java/com/unboundid/scim2/server/utils/SchemaCheckFilterVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2024 Ping Identity Corporation
+ * Copyright 2017-2025 Ping Identity Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
