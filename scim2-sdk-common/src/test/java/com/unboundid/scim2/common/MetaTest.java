f64a161ce4245d21dc5e94de0e0cdfb611b1a120
Khalid Qarryzada <khalidqarryzada@pingidentity.com>
2026-05-01 10:40:43 -0700

    Add utility methods to Meta.java.
    This commit updates the Meta class with convenience methods that
    simplify the interface for interaction with other data types. It is now
    possible to provide UNIX timestamps for created/lastModified
    timestamps, and location values may be set or fetched as strings.
    
    Reviewer: vyhhuang
    
    JiraIssue: DS-51461

--- scim2-sdk-common/src/test/java/com/unboundid/scim2/common/MetaTest.java
+++ scim2-sdk-common/src/test/java/com/unboundid/scim2/common/MetaTest.java
@@ -33,9 +33,11 @@
 package com.unboundid.scim2.common;
 
 import com.unboundid.scim2.common.types.Meta;
+import com.unboundid.scim2.common.utils.JsonUtils;
 import org.testng.annotations.Test;
 
 import java.net.URI;
+import java.time.Duration;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -48,30 +50,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MetaTest
 {
   /**
-   * Tests that a Meta object can be created with the builder pattern.
+   * This method provides basic validation for Meta objects.
    */
   @Test
-  public void testBuilderPattern() throws Exception
+  public void testBasic() throws Exception
   {
     final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    calendar.clear();
     calendar.set(1970, Calendar.JANUARY, 1, 11, 0);
 
     // The class-level Javadoc of the Meta class contains an example JSON.
     // These values are based on the ones from that example.
     Meta meta = new Meta()
         .setResourceType("User")
-        .setCreated(calendar)
+        .setCreatedMillis(0L)
         .setLastModified(calendar)
         .setLocation(new URI("https://example.com/v2/Users/fa1afe1"))
-        .setVersion("W/\"e180ee84f0671b1\"");
+        .setVersion("e180ee84f0671b1");
 
     assertThat(meta.getResourceType()).isEqualTo("User");
-    assertThat(meta.getCreated()).isEqualTo(calendar);
+    assertThat(meta.getCreated()).isNotNull();
+    assertThat(meta.getCreated().getTimeInMillis()).isEqualTo(0L);
     assertThat(meta.getLastModified()).isEqualTo(calendar);
-    assertThat(meta.getLocation()).isNotNull();
-    assertThat(meta.getLocation().toString())
+    assertThat(meta.getLocationString())
         .isEqualTo("https://example.com/v2/Users/fa1afe1");
-    assertThat(meta.getVersion()).isEqualTo("W/\"e180ee84f0671b1\"");
+    assertThat(meta.getVersion()).isEqualTo("e180ee84f0671b1");
+
+    // Ensure that the JSON results in an expected form.
+    String metaJson = JsonUtils.getObjectWriter().writeValueAsString(meta);
+    String expected = """
+        {
+          "resourceType": "User",
+          "created": "1970-01-01T00:00:00Z",
+          "lastModified": "1970-01-01T11:00:00Z",
+          "location": "https://example.com/v2/Users/fa1afe1",
+          "version": "e180ee84f0671b1"
+        }""";
+    String expectedJson = JsonUtils.getObjectReader().readTree(expected)
+        .toString();
+    assertThat(metaJson).isEqualTo(expectedJson);
+
+    // Ensure the lastModified value can also be set with millisecond values.
+    long unixTimestamp = Duration.ofHours(11).toMillis();
+    Meta metaMillis = new Meta()
+        .setResourceType("User")
+        .setCreatedMillis(0L)
+        .setLastModifiedMillis(unixTimestamp)
+        .setLocationString("https://example.com/v2/Users/fa1afe1")
+        .setVersion("e180ee84f0671b1");
+    assertThat(metaMillis).isEqualTo(meta);
   }
 
   /**
@@ -94,5 +121,47 @@ public class MetaTest
     // Ensure the values were set properly.
     assertThat(object.getResourceType()).isEqualTo("Custom");
     assertThat(object.customField).isEqualTo("present");
+  }
+
+  /**
+   * Tests for {@code equals()}.
+   */
+  @Test
+  @SuppressWarnings("all")
+  public void testEquals()
+  {
+    Meta first = new Meta()
+        .setResourceType("User")
+        .setCreatedMillis(0L)
+        .setLastModifiedMillis(10L)
+        .setLocationString("https://example.com/v2/Users/fa1afe1");
+    Meta second = new Meta()
+        .setResourceType("User")
+        .setCreatedMillis(0L)
+        .setLastModifiedMillis(10L)
+        .setLocationString("https://example.com/v2/Users/fa1afe1");
+
+    assertThat(first.equals(second)).isTrue();
+    assertThat(first.equals(first)).isTrue();
+    assertThat(first.hashCode()).isEqualTo(second.hashCode());
+    assertThat(first.equals(null)).isFalse();
+    assertThat(first.equals(new Object())).isFalse();
+
+    assertThat(second.setCreatedMillis(10L)).isNotEqualTo(first);
+    assertThat(second.hashCode()).isNotEqualTo(first.hashCode());
+    second.setCreatedMillis(first.getCreated().getTimeInMillis());
+
+    assertThat(second.setLastModifiedMillis(1000L)).isNotEqualTo(first);
+    second.setLastModified(first.getLastModified());
+
+    assertThat(second.setLocationString(null)).isNotEqualTo(first);
+    assertThat(second.getLocationString()).isNull();
+    second.setLocationString(first.getLocationString());
+
+    assertThat(second.setResourceType("Other")).isNotEqualTo(first);
+    second.setResourceType("User");
+
+    assertThat(second.setVersion("213")).isNotEqualTo(first);
+    second.setVersion(null);
   }
 }
